dd583e3ba76d8bd9f3e5ebee68799f0756277d85
username <lara.viquiera@gmail.com>
2024-02-02 15:51:00 -0800

    complete logout function

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+import requests
 
 # Create your views here.
 def home(request):
