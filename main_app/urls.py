9da9943a37d748c2d7dc7c95cda6ecb83cd3c648
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-02-02 14:22:34 -0800

    initalize models

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
     
 urlpatterns = [
-    path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('feed/', views.feed_index, name='index'),
   ]
