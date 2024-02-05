8b10a960c94567063335365fd58a28e3804e0dd9
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-02-05 10:22:20 -0800

    Add model

--- main_app/models.py
+++ main_app/models.py
@@ -5,3 +5,12 @@ from django.contrib.auth.models import User
 class Feed(models.Model):
   pass
 
+class Post(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  name = models.CharField(max_length=50)
+  comment = models.TextField(max_length=250)
+  brand = models.CharField(max_length=50)
+  price = models.IntegerField()
+  url = models.URLField()
+  image = models.CharField()
+
